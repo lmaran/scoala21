1a565b67999835f577fa6c74a67abfe2e1708b13
lmaran <lucian.maran@outlook.com>
2019-01-01 18:49:08 +0000

    load browser-sync only in dev mode [skip ci]

--- src/server.js
+++ src/server.js
@@ -4,13 +4,13 @@ const http = require("http");
 const app_1 = require("./app");
 const config_1 = require("./config");
 const logger_1 = require("./logger");
-const browserSync = require("browser-sync");
 const httpServer = http.createServer(app_1.default);
 httpServer.listen(config_1.default.port);
 httpServer.on("listening", () => {
     const addr = httpServer.address();
     logger_1.default.warn(`Express server listening on port ${addr.port} in ${config_1.default.env} mode (pid: ${process.pid})`);
     if (`${config_1.default.env}` === "development") {
+        const browserSync = require("browser-sync");
         browserSync({
             files: ["dist/src/**/*.{html,js,css,hbs}"],
             online: true,
