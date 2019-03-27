68908e659adf53582504dbbe684f875d876bd2b9
Lucian Maran <lucian.maran@outlook.com>
2019-03-27 22:43:31 +0200

    add timetable for classes

--- server/src/web/controllers/class.controller.js
+++ server/src/web/controllers/class.controller.js
@@ -3,7 +3,7 @@ const lessonService = require("../services/lesson.service");
 const timetableService = require("../services/timetable.service");
 const { PageNotFound } = require("../../shared/errors/all.errors");
 const matemaratonService = require("../services/matemaraton.service");
-
+const arrayHelper = require("../../shared/helpers/array.helper");
 
 exports.getAll = async (req, res) => {
     const classes = await classService.getAll();
@@ -37,7 +37,6 @@ exports.getAll = async (req, res) => {
     });
 
     const data = {
-        // classesByGrade,
         classesByGrade,
         ctx: req.ctx
     };
@@ -74,7 +73,6 @@ exports.getTeachers = async (req, res) => {
     ]);
 
     const data = {
-        // classesByGrade,
         class: cls,
         lessons,
         ctx: req.ctx
@@ -119,15 +117,138 @@ exports.getTimetable = async (req, res, next) => {
         await timetableService.getTimetableItemsForClass(timetableId, classId)
     ]);
 
+    const lessonsAsObject = arrayHelper.arrayToObject(lessons, "_id");
+
+    const timetableItemsAsObject = timetableItems.reduce((acc, crt) => {
+        const crtLesson = lessonsAsObject[crt.lessonId];
+        const newItem = {
+            day: crt.day,
+            hour: crt.hour,
+            lesson: {
+                subject: crtLesson.subject,
+                teacher: crtLesson.teacher
+            },
+            allWeeks: !crtLesson.week
+        };
+
+        if (crtLesson.week) {
+            newItem.lesson.week = crtLesson.week;
+        }
+
+        if (acc[crt.day]) {
+            acc[crt.day].push(newItem);
+        } else {
+            acc[crt.day] = [newItem];
+        }
+        return acc;
+    }, {});
+
+    // if there are multiple lessons per hour (allWeeks=false) then group them in a single object
+    // with property "lessons" instead of "lesson"
+    Object.keys(timetableItemsAsObject).forEach(key => {
+        timetableItemsAsObject[key] = timetableItemsAsObject[key].reduce((acc, crt) => {
+            if (crt.allWeeks) {
+                acc.push(crt);
+            } else {
+                const findTimeItem = acc.find(x => x.hour === crt.hour);
+                const newLessonPerWeek = {
+                    week: crt.lesson.week,
+                    lesson: {
+                        subject: crt.lesson.subject,
+                        teacher: crt.lesson.teacher
+                    }
+                };
+                if (findTimeItem) {
+                    findTimeItem.lessonsPerWeek.push(newLessonPerWeek);
+                    // findTimeItem.lessonsPerWeek = findTimeItem.lessonsPerWeek.sort(sortByWeekName);
+                } else {
+                    acc.push({
+                        day: crt.day,
+                        // dayNumber: crt.dayNumber,
+                        hour: crt.hour,
+                        lessonsPerWeek: [newLessonPerWeek],
+                        allWeeks: crt.allWeeks // always false here
+                    });
+                }
+            }
+            return acc;
+        }, []);
+    });
+
+    const allHours = [
+        {
+            id: "1",
+            start: "08:00",
+            end: "08:50"
+        },
+        {
+            id: "2",
+            start: "09:00",
+            end: "09:50"
+        },
+        {
+            id: "3",
+            start: "10:00",
+            end: "10:45"
+        },
+        {
+            id: "4",
+            start: "11:00",
+            end: "11:50"
+        },
+        {
+            id: "5",
+            start: "12:00",
+            end: "12:50"
+        },
+        {
+            id: "6",
+            start: "13:00",
+            end: "13:50"
+        }
+    ];
+
+    const allDays = ["Luni", "Marti", "Miercuri", "Joi", "Vineri"];
+    const lessonsPerDay = [];
+
+    allDays.forEach(day => {
+        const lessonsPerHour = timetableItemsAsObject[day];
+
+        const newLessonsPerHour = [];
+        allHours.forEach(hour => {
+            const existingLessonPerHour = lessonsPerHour.find(x => x.hour === hour.id);
+            const newLessonPerHour = {
+                hour: hour
+            };
+            if (existingLessonPerHour) {
+                newLessonPerHour.allWeeks = existingLessonPerHour.allWeeks;
+                if (existingLessonPerHour.allWeeks) {
+                    newLessonPerHour.lesson = existingLessonPerHour.lesson;
+                } else {
+                    // multiple lessons per hour
+                    newLessonPerHour.lessonsPerWeek = existingLessonPerHour.lessonsPerWeek;
+                }
+            } else {
+                newLessonPerHour.noLesson = true;
+            }
+
+            newLessonsPerHour.push(newLessonPerHour);
+        });
+
+        lessonsPerDay.push({
+            day: day,
+            lessonsPerHour: newLessonsPerHour
+        });
+    });
+
     const data = {
-        lessons,
         class: cls,
-        timetableItems,
+        lessonsPerDay,
         ctx: req.ctx
     };
 
-    res.send(data);
-    //res.render("class/class-timetable", data);
+    // res.send(data);
+    res.render("class/class-timetable", data);
 };
 
 exports.getClass = async (req, res) => {
@@ -161,6 +282,18 @@ exports.getClass = async (req, res) => {
     // res.send(data);
     res.render("class/class", data);
 };
+
+// const getDayNumber = day => {
+//     if (day === "Luni") return 1;
+//     else if (day === "Marti") return 2;
+//     else if (day === "Miercuri") return 3;
+//     else if (day === "Joi") return 4;
+//     else if (day === "Vineri") return 5;
+//     else if (day === "Sambata") return 6;
+//     else return 7;
+// };
+
+// const sortByWeekName = (a, b) => (a.week.shortName > b.week.shortName ? 1 : -1);
 
 // item in DB (sample):
 // {
